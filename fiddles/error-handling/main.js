f6a71b80fa3eacaa4b495a6fbb6a2657a800066b
Marc Solà Nadal <marcsolanadal@gmail.com>
2017-05-10 21:27:02 +0200

    shape forming correctly and displaying error list

--- fiddles/error-handling/main.js
+++ fiddles/error-handling/main.js
@@ -1,5 +1,5 @@
 const { Component } = React
-const { createStore, applyMiddleware, combineReducers, compose } = Redux
+const { createStore, applyMiddleware, combineReducers, compose, bindActionCreators } = Redux
 const { Provider, connect } = ReactRedux
 
 
@@ -40,10 +40,12 @@ const thunk = store => next => action =>
         body: 'This is an error of type 1'
       }
     },
+    allIds: ['1', '2', '3'],
     byType: {
       'TYPE1_ERROR': ['1', '3'],
       'TYPE2_ERROR': ['2']
-    }
+    },
+    allTypes: ['TYPE1_ERROR', 'TYPE2_ERROR']
   }
 }
 */
@@ -60,26 +62,19 @@ const thunk = store => next => action =>
 }
 */
 
-// Closure to return next id
-// TODO: Check how to generate immutable ids inside reducer
-const nextId = ((id = 0) => {
-  return () => id += 1
-})()
-
 // This slice reducer takes the error and adds it to the byId object.
 // It takes the id of the error from the parent error reducer.
-const errorById = (state, action, id) => {
-  switch(action.error) {
-    case true:
-      return {
-        ...state.byId,
-        [id]: {
-          id,
-          type: action.type,
-          header: action.payload.header,
-          body: action.payload.body
-        }
-      }
+const errorById = (state = {}, action) => {
+  const { type, payload } = action
+  const { id, header, body } = payload
+  return {
+    ...state,
+    [id]: {
+      id,
+      type,
+      header,
+      body
+    }
   }
 }
 
@@ -87,25 +82,40 @@ const errorById = (state, action, id) => {
 // It creates an array for every new type of action that triggers an error.
 // The id comes from the parent error reducer.
 // TODO: See how to avoid losing the ids of the previous errors for each type.
-const errorByType = (state, action, id) => {
-  switch(action.error) {
-    case true:
-      return {
-        ...state.byType,
-        [action.type]: [
-          id
-        ]
-      }
+const errorByType = (state = {}, action) => {
+  return {
+    ...state,
+    [action.type]: addId(state, action)
   }
 }
 
-const errorReducer = (state = {}, action) => {
+const addId = (state = [], action) => {
+  return [
+    ...state[action.type] || [],
+    action.payload.id
+  ]
+}
+
+const allIds = (state = [], action) => {
+  return [
+    ...state,
+    action.payload.id
+  ]
+}
+
+const defaultState = {
+  byIds: {},
+  allIds: [],
+  byType: []
+}
+
+const errorReducer = (state = defaultState, action) => {
   switch(action.error) {
     case true:
-      const id = nextId().toString()
       return {
-        byId: errorById(state, action, id),
-        byType: errorByType(state, action, id)
+        byId: errorById(state.byId, action),
+        allIds: allIds(state.allIds, action),
+        byType: errorByType(state.byType, action)
       }
 
     default:
@@ -117,11 +127,15 @@ const rootReducer = combineReducers({
   errors: errorReducer
 })
 
+const selectAllErrors = (state) => {
+  return state.errors.allIds.map(id => state.errors.byId[id])
+}
+
 // TODO: Find a better way to get the last item of the array
-const selectAllErrors = (state) => state.byId
-const selectLastFromType = (type) => {
-  const arrayCopy = state.byType[type]
-  return arrayCopy.pop()
+const selectLastFromType = (state, type) => {
+  const errorsByType = state.errors.byType[type]
+  const id = errorsByType[errorsByType.length - 1]
+  return state.errors.byId[id]
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -129,41 +143,82 @@ const store = createStore(
   rootReducer,
   composeEnhancers(
     applyMiddleware(...[
-      thunk,
-      logger
+      thunk
     ])
   )
 )
 
-const triggerErrorAction = (num) => {
+// Closure to return next id
+// TODO: Check how to generate immutable ids inside reducer
+const nextId = ((id = 0) => {
+  return () => id += 1
+})()
+
+const fireError = (num) => {
   return {
     type: `ERROR_TYPE${num}`,
     error: true,
     payload: {
+      id: nextId().toString(),
       header: `BOOM TYPE ${num}`,
       body: `An error of type ${num} has occurred T-T`
     }
   }
 }
 
+// TODO: Error header
+
+// TODO: Error description
+
 class App extends Component {
-  componentDidMount() {
-    store.dispatch(triggerErrorAction(1))
-    store.dispatch(triggerErrorAction(2))
-    store.dispatch(triggerErrorAction(2))
-    store.dispatch(triggerErrorAction(1))
-    store.dispatch(triggerErrorAction(3))
+  getRandomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  handleClick = () => {
+    this.props.actions.fireError(this.getRandomInt(0, 5))
   }
   render() {
     return (
       <div>
-        May the force be with you
+        <button onClick={this.handleClick}>Fire error</button>
+        <ul>
+          {
+            this.props.errors.map(error => (
+              <li key={error.id}>
+                <div>
+                  <b>{error.header}</b>:
+                  <p>{error.body}</p>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    //errors: selectAllErrors(state),
+    errors: selectLastFromType(state, 'ERROR_TYPE2')
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators({
+      fireError
+    }, dispatch)
+  }
+
+}
+
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>,
   document.getElementById('root')
 )
