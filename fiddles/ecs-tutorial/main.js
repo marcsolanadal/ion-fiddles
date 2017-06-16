b15fda99daef82c364a0bff75ea1c11b92ed5ee2
marcsolanadal <marcsolanadal@gmail.com>
2017-06-16 14:18:27 +0200

    WIP: Implemetation of the Entities

--- fiddles/ecs-tutorial/main.js
+++ fiddles/ecs-tutorial/main.js
@@ -1,58 +1,44 @@
 let ECS = {}
 
-ECS.Entity = function Entity () {
-
-  this.id = (+new Date().toString(16)
-    + Math.random() * 1000000 | 0)
-    + ECS.Entity.prototype._count
-
-  ECS.Entity.prototype._count + 1
-
-  this.components = {}
-
-  return this
-}
-
-ECS.Entity.prototype._count = 0;
-
-
-const Entity = () => {
+ECS.Entity = () => {
   const id = (+new Date().toString(16)
     + Math.random() * 1000000 | 0)
-    + ECS.Entity.prototype._count
 
   let components = {}
 
-  const addComponent = (component) => {
-    this.components[component.name] = component
-    return components
+  const curry = (fn) => {
+    return (params) => {
+      fn(params)
+      return {
+        id,
+        components
+      }
+    }
+  }
+
+  const addComponent = (comp) => {
+    components[comp.name] = comp
   }
 
   const removeComponent = (name) => {
-    this.components = components.filter(component => component.name !== name)
+    components = components.filter(component => component.name !== name)
   }
 
-
+  const print = () => {
+    console.log(id, components)
   }
+
+  return {
+    addComponent: curry(addComponent),
+    removeComponent: removeComponent,
+    print
+  }
+
 }
-.removeComponent = function removeComponent ( componentName ){
-    // Remove component data by removing the reference to it.
-    // Allows either a component function or a string of a component name to be
-    // passed in
-    var name = componentName; // assume a string was passed in
 
-    if(typeof componentName === 'function'){
-        // get the name from the prototype of the passed component function
-        name = componentName.prototype.name;
-    }
+console.log('ECS', ECS)
 
-    // Remove component data by removing the reference to it
-    delete this.components[name];
-    return this;
-};
-
-ECS.Entity.prototype.print = function print () {
-    // Function to print / log information about the entity
-    console.log(JSON.stringify(this, null, 4));
-    return this;
-};
+const entity = ECS.Entity();
+const test = entity.addComponent({ name: 'HEALTH', foo: 'bar' })
+console.log('test', test);
+entity.print();
